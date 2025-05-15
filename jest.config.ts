0cde8a9b701690d699f23cd0368055a5c0a0c18a
santhoshsakleshpur <santhosh.sakleshpur@publicissapient.com>
2025-05-15 11:42:23 +0100

    test: add test cases

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json'
+    }
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+};
+
+export default config;
